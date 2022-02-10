6b006c09eafbb9dc32a2dd83eb96187b9fd8b3f4
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-02-10 14:06:30 -0600

    Declaring types for use state as well as events in typescript

--- components/Person.tsx
+++ components/Person.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react'
+import {FC, ChangeEvent,useState} from 'react'
 
 //declaring the types of props in a component
 interface PersonProps {
@@ -9,11 +9,21 @@ interface PersonProps {
 
 //declaring the type of props as a function argument
 export const Person1 =(props: PersonProps)=>{
+
+    //Declaring types for useState in Typescript
+    const [country,setCountry] = useState<string | null>('');
+
+    //declaring the type of event in typescript
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCountry(event.target.value);
+    }
     return(
         <div>
             <h1>{props.name}</h1>
             <h1>{props.age}</h1>
             <h1>{props.email}</h1>
+            <input placeholder='Write down your country' onChange={handleChange}></input>
+            <h1>{country}</h1>
 
         </div>
     )
