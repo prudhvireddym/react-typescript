24a9237ed65a29e60ecec752d803c97a9f37bb55
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-02-10 11:13:40 -0600

    Adding Person component and passing props with types in two diffrent ways

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React,{FC} from 'react';
 
 import './App.css';
-import { PersonDetails } from './components/Person';
+import { Person1,Person2 } from './components/Person';
 
 //Function Component is defined using the FC keyword
 const App: FC = ()=> {
@@ -20,7 +20,8 @@ const App: FC = ()=> {
   return (
     <div className="App">
       <p> {name} is {age} years old and {isMarried ? 'married' : 'not married'}</p>
-      <PersonDetails name='Prudhvi' age={20} email={'prudhviemail@gmail.com'} />
+      <Person1 name='Prudhvi' age={20} email={'prudhviemail@gmail.com'} />
+      <Person2 name='Prudhvi Reddy' age={25} email={'prudhviemail2@gmail.com'} />
     </div>
   );
 }
--- components/Person.tsx
+++ components/Person.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import {FC} from 'react'
 
 //declaring the types of props in a component
 interface PersonProps {
@@ -8,7 +8,20 @@ interface PersonProps {
 }
 
 //declaring the type of props as a function argument
-export const PersonDetails =(props: PersonProps)=>{
+export const Person1 =(props: PersonProps)=>{
+    return(
+        <div>
+            <h1>{props.name}</h1>
+            <h1>{props.age}</h1>
+            <h1>{props.email}</h1>
+
+        </div>
+    )
+}
+
+//Better way to do the above code is both the ways work
+
+export const Person2: FC<PersonProps> = (props)=>{
     return(
         <div>
             <h1>{props.name}</h1>
