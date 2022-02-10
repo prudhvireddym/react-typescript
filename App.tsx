db5b70558959f5bd40844a6dd8ec4516460e3a97
Prudhvi Reddy <prudhvireddy@Prudhvis-MacBook-Air.local>
2022-02-10 15:43:08 -0600

    Context Api decalring etc in typescript

--- App.tsx
+++ App.tsx
@@ -1,13 +1,28 @@
-import React,{FC} from 'react';
+import React,{FC,createContext} from 'react';
 
 import './App.css';
 import { Person1,Person2,HairColour } from './components/Person';
+
+
+interface AppContextInterface{
+    name:string;
+    age:number;
+    email:string;
+}
+
+const AppContext = createContext<AppContextInterface | null> (null)
 
 //Function Component is defined using the FC keyword
 const App: FC = ()=> {
   const name: string = 'Prudhvi';
   const age: number = 25;
   const isMarried: boolean = false;
+
+  const contextValue : AppContextInterface = {
+    name: "Prudhvi",
+    age: 25,
+    email: "prudhviemail@gmail.com"
+  }
 
   const getAge = (name:string):number =>{
     if (name==='prudhvi'){
@@ -18,11 +33,13 @@ const App: FC = ()=> {
 }
 
   return (
+    <AppContext.Provider value ={contextValue}>
     <div className="App">
       <p> {name} is {age} years old and {isMarried ? 'married' : 'not married'}</p>
       <Person1 name='Prudhvi' age={20} email={'prudhviemail@gmail.com'} hairColour={HairColour.Red}/>
       <Person2 name='Prudhvi Reddy' age={25} email={'prudhviemail2@gmail.com'} />
     </div>
+    </AppContext.Provider>
   );
 }
 
